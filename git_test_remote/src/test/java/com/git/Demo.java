6b305d58a46423a24e201d5988ce96f36fb7dd24
HuoXinYongHen <452528098@qq.com>
2018-10-09 00:22:13 +0800

    A程序员添加demo

---
+++ git_test_remote/src/test/java/com/git/Demo.java
@@ -0,0 +1,14 @@
+package com.git;
+
+/**
+ * @author yanxinzhang
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/10/90:18
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("这是A程序员添加的");
+    }
+}
