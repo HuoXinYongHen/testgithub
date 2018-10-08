ad54960d2621218c444cad45fc9d96ad1ad2b7ff
HuoXinYongHen <452528098@qq.com>
2018-10-08 21:21:58 +0800

    项目经理第一次上传项目

---
+++ git_test_remote/src/main/java/com/git/controller/testGit.java
@@ -0,0 +1,15 @@
+package com.git.controller;
+
+/**
+ * @author yanxinzhang
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/10/820:54
+ */
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试Git");
+    }
+
+}
