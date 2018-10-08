ead80f654980821187928378dfd33e1b96c7fa80
HuoXinYongHen <452528098@qq.com>
2018-10-09 00:34:09 +0800

    A程序员添加制造冲突

--- git_test_remote/src/test/java/com/git/Demo.java
+++ git_test_remote/src/test/java/com/git/Demo.java
@@ -11,5 +11,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("这是A程序员添加的");
         System.out.println("这是B程序员修改的");
+        System.out.println("A程序员修改第一02");
     }
 }
